30adac10cf1297d4c0c334a8c4b1954cf32214a9
ryan chia <ryanchia@ryans-MacBook-Pro.local>
2025-07-18 11:01:36 +0800

    week8 changes

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import pandas as pd
 # from helper_functions import llm
 from logics.customer_query_handler import process_user_message
 
+from helper_functions.utility import check_password
+
 
 # region <--------- Streamlit App Configuration --------->
 st.set_page_config(
     layout="centered",
     page_title="My Streamlit App"
 )
+
+# Check if the password is correct.
+if not check_password():
+    st.stop()
+
 # endregion <--------- Streamlit App Configuration --------->
 
 st.title("Streamlit App")
@@ -20,7 +27,7 @@ form.subheader("Prompt")
 user_prompt = form.text_area("Enter your prompt here", height=200)
 
 if form.form_submit_button("Submit"):
-    
+
     st.toast(f"User Input Submitted - {user_prompt}")
 
     st.divider()
@@ -32,4 +39,4 @@ if form.form_submit_button("Submit"):
 
     print(course_details)
     df = pd.DataFrame(course_details)
-    df 
+    df
